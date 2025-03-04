a108beac1a8536624b342d208066564a2f315099
Isaac <isaactraba@gmail.com>
2025-03-04 11:42:56 +0100

    first commit (Skelleton + first component)

---
+++ src/app/movie-item/movie-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { Movie } from '../model/movie.model';
+import { MoneyPipe } from '../pipes/moneyPipe/money.pipe';
+import { DurationPipe } from '../pipes/durationPipe/duration.pipe';
+
+
+@Component({
+  selector: 'app-movie-item',
+  templateUrl: 'movie-item.component.html',
+  standalone: true,
+  styleUrls: [ 'movie-item.component.scss' ],
+  imports: [MoneyPipe, DurationPipe]
+})
+export class MovieItemComponent {
+
+  @Input() movie: Movie;
+}
+
---
+++ src/app/pipes/durationPipe/duration.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'durationPipe'
+})
+export class DurationPipe implements PipeTransform {
+
+  transform(value: number): string {
+    let hours = 0;
+    let minutes = 0;
+
+    hours = Math.floor(value / 60);
+    value = value - (hours*60);
+    minutes = value;
+
+    return hours + 'h ' + minutes + 'min ';
+  }
+}
