919af3fdb32b635dc5e4f0df933d4bdd45e77d21
Isaac <isaactraba@gmail.com>
2025-04-01 17:53:52 +0200

    home creation + routing from app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,16 @@
-import { Routes } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
 
 export const routes: Routes = [
-
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { 
+        path: 'home', 
+        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) 
+    },
+    {
+        path: 'movie-details/:id',
+        loadComponent: () => import('./features/movie-details/movie-details.component').then(m => m.MovieDetailsComponent)
+    },
+    { path: '**', redirectTo: 'home' },
 ];
+
+export const appRouting = provideRouter(routes);
